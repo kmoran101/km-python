836d65f71c50b631daf41d430fcff66156adefa0
Kevin Moran <kmoran40@gmail.com>
2020-09-18 21:31:11 -0400

    KM Test commit

---
+++ Morse/Morse.py
@@ -0,0 +1,12 @@
+import winsound
+
+frequency = 880
+dit = 200
+dash = 500
+
+winsound.Beep(frequency, dit)
+winsound.Beep(frequency, dit)
+winsound.Beep(frequency, dit)
+winsound.Beep(frequency, dash)
+winsound.Beep(frequency, dash)
+winsound.Beep(frequency, dash)
