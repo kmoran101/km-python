10db1231892af48e9226343886db494292d2a300
Kevin Moran <kmoran40@gmail.com>
2020-09-18 22:53:51 -0400

    KM-more-progress

--- Morse/Morse.py
+++ Morse/Morse.py
@@ -1,12 +1,155 @@
 import winsound
 
-frequency = 880
-dit = 200
-dash = 500
 
-winsound.Beep(frequency, dit)
-winsound.Beep(frequency, dit)
-winsound.Beep(frequency, dit)
-winsound.Beep(frequency, dash)
-winsound.Beep(frequency, dash)
-winsound.Beep(frequency, dash)
+def dit():
+	winsound.Beep(880, 200)
+
+def dash():
+	winsound.Beep(880, 800)
+
+def a():
+	dit()
+	dash()
+
+def b():
+	dash()
+	dit()
+	dit()
+	dit()
+
+def c():
+	dash()
+	dit()
+	dash()
+	dit()
+
+def d():
+	dash()
+	dit()
+	dit()
+
+def e():
+	dit()
+
+def f():
+	dit()
+	dit()
+	dash()
+	dit()
+
+def g():
+	dash()
+	dash()
+	dit()
+
+def h()
+	dit()
+	dit()
+	dit()
+	dit()
+
+def i():
+	dit()
+	dit()
+
+def j():
+	dit()
+	dash()
+	dash()
+	dash()
+
+def k():
+	dash()
+	dit()
+	dash()
+
+def l():
+	dit()
+	dash()
+	dit()
+	dit()
+
+def m():
+	dash()
+	dash()
+
+def n():
+	dash()
+	dit()
+
+def o():
+	dash()
+	dash()
+	dash()
+
+def p():
+	dit()
+	dash()
+	dash()
+	dit()
+
+def q():
+	dash()
+	dash()
+	dit()
+	dash()
+
+def r():
+	dit()
+	dash()
+	dit()
+
+def s():
+	dit()
+	dit()
+	dit()
+
+def t():
+	dash()
+
+def u():
+	dit()
+	dit()
+	dash()
+
+def v():
+	dit()
+	dit()
+	dit()
+	dash()
+
+def w():
+	dit()
+	dash()
+	dash()
+
+def x():
+	dash()
+	dit()
+	dit()
+	dash()
+
+def y():
+	dash()
+	dit()
+	dash()
+	dash()
+
+def z():
+	dash()
+	dash()
+	dit()
+
+
+a()
+b()
+c()
+
+
+
+
+
+
+
+
+
